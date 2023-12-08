d9780792899ee235a8d6ae068fd4304e97f0afb9
Thaofoo <mthafixh@gmail.com>
2023-12-08 15:01:05 +0700

    repisi dikit

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,7 +2,7 @@
     <a href="{{ URL::route('landing') }}" class="logo"><img src="{{ URL::to('/') }}/image/logo.png" alt="" /></a>
     <div class="searchbar">
       <form method="GET">
-        <div class="input-form text-black bg-gray-200 p-2 rounded-2xl border border-black flex flex-row h-12 w-[50rem] flex items-center justify-start">
+        <div class="input-form text-black bg-gray-200 p-2 rounded-2xl border border-black flex flex-row h-12 w-[50rem] items-center justify-start">
           <i class="fa-solid fa-magnifying-glass ml-2"></i>
           <input type="text" id="form" placeholder="Cari" class="p-0 h-[1.7rem] w-[43.75rem] bg-gray-200 text-black placeholder-black ml-3 border-none" />
         </div>
